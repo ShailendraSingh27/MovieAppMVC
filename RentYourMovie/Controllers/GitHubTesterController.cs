5230493825fb1c2168e310a6873da2fcd20952b2
Shailendra Singh <ShailendraSingh27@users.noreply.github.com>
2018-12-21 19:03:42 +0530

    testing commits

---
+++ RentYourMovie/Controllers/GitHubTesterController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentYourMovie.Controllers
+{
+    public class GitHubTesterController : Controller
+    {
+        //
+        // GET: /GitHubTester/
+        public ActionResult Index()
+        {
+            //comment code
+            return View();
+        }
+	}
+}
