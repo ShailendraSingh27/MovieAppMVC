aa7c13513c7d2b4eb18549e885c5727a8ded9a45
Shailendra Singh <ShailendraSingh27@users.noreply.github.com>
2019-01-02 17:32:15 +0530

    Install AutoMapper Package

---
+++ RentYourMovie/DTOs/CustomerDto.cs
@@ -0,0 +1,27 @@
+﻿using RentYourMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RentYourMovie.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Customer's name is required.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [Minimum18YearsIfMember]
+        public DateTime? BirthDate { get; set; }
+        public bool IsSubscribeToNewsLetter { get; set; }
+
+
+        //Membership Type
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+    }
+}
